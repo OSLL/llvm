fea38ea35f91194f1d01d6049b15b5c3d5a24a51
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2013-01-23 01:56:50 +0400

    test suite: introduced the support of seed values
    The test script is modified to supply a test driver with
    a random seed value and then compare the output of the reference
    and testing programs. The test driver generates a test
    from this value that makes it possible to test the reconstructed
    program on different pseudorandom inputs.

--- test/main-matrix.c
+++ test/main-matrix.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 extern int mat_mult(int*, int*, int*);
 
-int main() {
-	int A[] = {	1, 0, 0,
-				0, 1, 0,
-				0, 0, 1	};
 
-	int B[] = {	2, 0, 0,
-				0, 2, 0,
-				0, 0, 2	};
-
-	int C[9];
+static void print_matrix3(int* m) {
 	int i, j;
-
-
-	mat_mult(A, B, C);
-
 
 	for (i = 0; i < 3; ++i) {
 		for (j = 0; j < 3; ++j) {
-			printf("%d ", C[i*3 + j]);
+			printf("%d ", m[i*3 + j]);
 		}
 
 		printf("\n");
 	}
+}
+
+int main(int argc, char** argv) {
+	int A[9];
+	int B[9];
+	int C[9];
+	int i;
+
+	if (argc < 2) {
+		printf("Usage: matrix-main.c <seed>\n");
+		return 1;
+	}
+
+	srand(atoi(argv[1]));
+
+	for (i = 0; i < 9; ++i) {
+		A[i] = rand() % 100;
+		B[i] = rand() % 100;
+	}
+
+	mat_mult(A, B, C);
+
+	printf("A is\n");
+	print_matrix3(A);
+
+	printf("\nB is\n");
+	print_matrix3(B);
+
+	printf("\nC is\n");
+	print_matrix3(C);
 
 	return 0;
 }
